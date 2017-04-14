956462a36b803f60d325431da896732815213cec
cn.shperry <cn.shperry@gmail.com>
2017-04-14 16:48:18 +0800

    optimize load configuration logic.

--- src/config/index.js
+++ src/config/index.js
@@ -6,15 +6,16 @@ import stripJsonComments from 'strip-json-comments'
 import {Provider} from 'nconf/lib/nconf/provider'
 
 const environment = process.env.NODE_ENV || 'local'
-const appPath = process.cwd()
 const nconf = new Provider()
 
+const globalConf = {
+  appPath: process.cwd(),
+  environment
+}
+
 nconf
-  .add('new', {type: 'literal', store: {ab: 'ab'}})
+  .add('global', {type: 'literal', store: globalConf})
   .add('app_env', {type: 'literal', store: JSON.parse(stripJsonComments(fs.readFileSync(path.join(__dirname, `${environment}.json`)).toString()))})
   .add('app_default', {type: 'literal', store: JSON.parse(stripJsonComments(fs.readFileSync(path.join(__dirname, 'default.json')).toString()))})
-
-nconf.set('appPath', appPath)
-nconf.set('environment', environment)
 
 export default nconf
