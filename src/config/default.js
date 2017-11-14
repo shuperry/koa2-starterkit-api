52d4964bd32a287908dbd9bb04b49434d4c4e387
cn.shperry <cn.shperry@gmail.com>
2017-11-14 15:20:12 +0800

    remove useless configuration.

--- src/config/default.js
+++ src/config/default.js
@@ -58,20 +58,5 @@ export default {
   },
   files: {
     maxUploadCount: 100
-  },
-  testTokens: {
-    '9eh87828yw7pybri94ims63zf3ivsy63': 'rtptest1',
-    'if5cuswa3765ojb4vl79a13bir4p7ezq': 'rtptest2',
-    '4gwm7ealbpzxvri8rxczubictt3o75n7': 'rtptest3',
-    '1k4fho7vmza4dm1liehmr0rxjsccmf82': 'rtptest4',
-    'iv7fra9649c80f824op7jnqs6wocu98r': 'rtptest5',
-    '0urt6q50gn2fqac6ajqhv0v5e0vmwbv2': 'rtptest6',
-    'yzlf9xylw3zpcflb7jogad1znfwvhd6s': 'rtptest7',
-    'vrarq3mrvz7we2t0bps7kalylrt245jx': 'rtptest8',
-    'phoajfg3oeet49e3l7ncmfl6c6y0lorf': 'rtptest9',
-    'ppj6zsab4vv86ijeyixwkk0wjydh8g1m': 'rtptest10',
-    'h6cgzmr4dqdhm1u4um2bofjtww0voix6': 'rtptest11',
-    'wsi6pwszd2ab8xvakth14w3ljk3dvfvs': 'rtptest12',
-    'bq9by6q0bbd0hozel4of621pxm5tf5y6': 'rtptest13'
   }
 }
